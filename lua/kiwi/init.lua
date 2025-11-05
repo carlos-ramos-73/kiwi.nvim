6cc91c0f1eab591181067e6eec3a1e89ddd0eb7b
Carlos Ramos <carlos.a.ramos.73@gmail.com>
2025-11-04 18:26:28 -0800

    Remove user commands. I changed my mind, I don't like it.

--- lua/kiwi/init.lua
+++ lua/kiwi/init.lua
@@ -9,16 +9,6 @@ local M = {}
 vim.keymap.set("n", "<Plug>(KiwiOpenWikis)", function() wiki.open_wiki_index() end)
 vim.keymap.set("n", "<Plug>(KiwiToggleTodo)", function() todo.toggle() end)
 
--- User Commands
-vim.api.nvim_create_user_command("KiwiOpenWiki", function(args)
-    wiki.open_wiki_index(args.args)
-  end,
-  {
-    nargs = "?",
-    desc = "Open wiki index",
-  })
-
--- Try to remove setup function below
 M.setup = function(opts)
   utils.setup(opts, config)
 end
